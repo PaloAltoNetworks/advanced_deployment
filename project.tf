0f8fabfb36b92dd130fba568ee0c88bfd3b48014
Shanmukh Kotharu <43720664+shanmukhkotharu@users.noreply.github.com>
2021-05-13 20:07:11 +0530

    Update project.tf

--- project.tf
+++ project.tf
@@ -1,5 +1,11 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12.3"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.67.0"
+    }
+  }
 }
 
 provider "google" {
